8a9923c72036834b7d4e3c83bb4edddc1f680274
DanielKarstens <dkhaha19@hotmail.com>
2018-02-05 12:56:23 +0100

    opg 1-3

---
+++ ClassLibrary1/ClassLibrary1/Bil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Bil : Køretøj
+    {
+        public string Nummerplade;
+        public DateTime Dato;
+
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string KøretøjType()
+        {
+            return "Bil";
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1Tests/BilTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void Pristest()
+        {
+            var Bil = new Bil();
+            int Pris = Bil.Pris();
+            Assert.AreEqual(240,Pris);
+        }
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            var Bil = new Bil();
+            string køretøj = Bil.Køretøj();
+            Assert.AreEqual("Bil" , køretøj);
+        }
+        [TestMethod()]
+        public void MCPristest()
+        {
+            var MC = new MC();
+            int Pris = MC.Pris();
+            Assert.AreEqual(125, Pris);
+        }
+        [TestMethod()]
+        public void MCKøretøjTest()
+        {
+            var MC = new MC();
+            string køretøj = MC.Køretøj();
+            Assert.AreEqual("MC", køretøj);
+        }
+    }
+}
